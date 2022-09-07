d314ab56e6db79c3910b11207ed6500330c833a9
teamssix <teamssix@163.com>
2022-09-07 21:53:27 +0800

    doc: update version

--- pkg/util/env/env.go
+++ pkg/util/env/env.go
@@ -3,6 +3,6 @@ package env
 const (
 	CFHomeEnvVar = "CF_HOME"
 	AppDirName   = ".config/cf"
-	Version      = "v0.3.4"
-	UpdateTime   = "2022.8.16"
+	Version      = "v0.4.0"
+	UpdateTime   = "2022.9.7"
 )
