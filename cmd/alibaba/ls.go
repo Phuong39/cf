1f4d8aaac71243158e8002de4812394c18887a7b
TeamsSix <49087564+teamssix@users.noreply.github.com>
2022-10-12 11:50:01 +0800

    docs: update readme

--- cmd/alibaba/ls.go
+++ cmd/alibaba/ls.go
@@ -19,7 +19,7 @@ func init() {
 	alibabaCmd.AddCommand(lsCmd)
 	lsCmd.Flags().StringVarP(&lsRegion, "region", "r", "all", "指定区域 ID (Specify region ID)")
 	lsCmd.PersistentFlags().BoolVar(&lsFlushCache, "flushCache", false, "刷新缓存，不使用缓存数据 (Refresh the cache without using cached data)")
-	lsCmd.Flags().BoolVarP(&lsAllRegions, "allRegions", "a", false, "使用所有区域，包括私有区域 (Use all regions, including private regions.)")
+	lsCmd.Flags().BoolVarP(&lsAllRegions, "allRegions", "a", false, "使用所有区域，包括私有区域 (Use all regions, including private regions)")
 }
 
 var lsCmd = &cobra.Command{
